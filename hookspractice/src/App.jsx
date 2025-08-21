d45f48d7ae67d14ff65024c714933a14715d433c
ankit singh <ankiitsingh21@gmail.com>
2025-08-22 05:05:05 +0530

    hooksPractice

---
+++ hookspractice/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+
+function App() {
+
+  return (
+    <>
+      <MyComponent/>
+    </>
+  )
+}
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={incrementCount}>Increment</button>
+    </div>
+  );
+}
+
+
+export default App
